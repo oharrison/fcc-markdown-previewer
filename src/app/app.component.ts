9626e084d9e6e6bcc7e017de7b375355df4550a9
Onel Harrison <me@onelharrison.com>
2017-01-17 12:52:05 -0500

    Use more specific selection in styles to prevent markdown id collisions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { MarkdownInputComponent } from './markdown-input';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
